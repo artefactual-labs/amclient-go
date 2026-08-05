658db73763bc56098c1e4eeaf7eb3dc72b0a4bdd
Jesús García Crespo <jesus@sevein.com>
2026-08-05 09:22:45 +0200

    Use gotest.tools assertions throughout
    Upgrade gotest.tools/v3 to v3.5.2 and replace the remaining manual test
    assertions with its assert helpers.

--- ingest_test.go
+++ ingest_test.go
@@ -19,10 +19,6 @@ func TestIngest_Hide(t *testing.T) {
 	})
 
 	payload, _, err := client.Ingest.Hide(ctx, "52dd0c01-e803-423a-be5f-b592b5d5d61c")
-	if err != nil {
-		t.Errorf("Ingest.Hide() returned error: %v", err)
-	}
-
 	assert.NilError(t, err)
-	assert.Equal(t, payload.Removed, true)
+	assert.Assert(t, payload.Removed)
 }
